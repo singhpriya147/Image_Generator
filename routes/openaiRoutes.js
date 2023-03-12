36af4130b91870af3119c60121eaa23b6cbd1cc1
priya <impriya840@gmail.com>
2023-03-12 15:30:10 +0530

    openAI_imageGenerator backend

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const{ generateImage} = require('../controllers/openaiControllers');
+const router=express.Router();
+
+router.post('/generateimage', generateImage);
+module.exports=router;
